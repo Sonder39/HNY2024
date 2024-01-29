7685de90b82da5e732a55d7af8a25738638d9a32
Sonder39 <CyberAnts@example.com>
2024-01-29 10:37:39 +0800

    新增http1, 在http基础上优化了该题的模式。

--- Web/http1/src/post/post.php
+++ Web/http1/src/post/post.php
@@ -1,64 +1,60 @@
-<!DOCTYPE html>
+<!Doctype html>
 <html lang="en">
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap Dark Theme from BootSwatch-->
-    <link href="assert/bootswatch/bootstrap.min.css" rel="stylesheet">
-    <link href="assert/logo_style.css" rel="stylesheet">
-    <link href="assert/padding_style.css" rel="stylesheet">
+    <link href="../assert/bootswatch/bootstrap.min.css" rel="stylesheet">
+    <link href="../assert/logo_style.css" rel="stylesheet">
+    <link href="../assert/padding_style.css" rel="stylesheet">
+    <link href="../assert/markdown.css" rel="stylesheet">
     <title>Sonder Blog - Index</title>
 </head>
 <body class="d-flex flex-column vh-100">
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="index.php">Blog</a>
+    <button class="btn btn-dark text-white-50 font-weight-bold" onclick="window.location.href='../index.php'">首页
+    </button>
 </nav>
 <div class="container mt-4">
     <div class="row">
-        <div class="col-md-8">
+        <div class="col-md-10">
             <div class="card bg-dark text-white mb-4">
                 <div class="card-body">
                     <div class="card-text">
                         <p class="text-white-50">
-                            最近爬虫的狂欢让Sonder服务器不堪重负😣，于是Sonder设置了5道关卡来限制猖狂的爬虫。</p>
-                        
+                            最近爬虫的狂欢让服务器不堪重负😣，于是Sonder设置了一些严格的校验来限制猖狂的爬虫。</p>
                         <ul class="list-group">
                             <li class="list-group-item d-flex justify-content-between align-items-center text-info fw-bold">
-                                POST parameter: { host: cs, port: sec }
-                                <span class="badge bg-primary rounded-pill">2</span>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-center text-info fw-bold">
-                                POST form: {target: 2024}
+                                只接受POST方法访问
                                 <span class="badge bg-primary rounded-pill">1</span>
                             </li>
                             <li class="list-group-item d-flex justify-content-between align-items-center text-info fw-bold">
-                                Authentication: auth=admin
+                                IP白名单：110.191.22.73 | 110.191.22.79 | 110.191.22.113
                                 <span class="badge bg-primary rounded-pill">1</span>
                             </li>
                             <li class="list-group-item d-flex justify-content-between align-items-center text-info fw-bold">
-                                User-Agent: Chrome
-                                <span class="badge bg-primary rounded-pill">1</span>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-center text-info fw-bold">
-                                X-Forwarded-For: 192.168.0.1
+                                代理服务器信息：主机名为lutalica.time、HTTP版本为1.0、类型为Squid 3.1
                                 <span class="badge bg-primary rounded-pill">1</span>
                             </li>
                         </ul>
-                        <br>
-                        <?php
-                        include "ret.php";
-                        error_reporting(0);
-                        $ret = getCheck();
-                        echo $ret;
-                        ?>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="col-md-4">
-            <div class="card my-4">
-                <p class="logo">CSSEC</p>
+        <div class="col-md-10">
+            <div class="card bg-dark text-white mb-4">
+                <div class="card-body">
+                    <?php
+                    include "../module/http.php";
+                    error_reporting(0);
+                    $ret = getCheck();
+                    echo $ret;
+                    ?>
+                </div>
+                <div class="card-footer text-right">
+                    <button class="btn btn-dark " onclick="window.location.href='../index.php'">首页</button>
+                </div>
             </div>
         </div>
     </div>
